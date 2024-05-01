9dde62b525250d22596d49718f916175c95f9cef
gingHUB <jyugal180.jy@gmail.com>
2024-05-02 03:56:30 +0530

    added image upload api

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const morgan = require("morgan");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
 const postRoutes = require("./routes/posts");
+const multer = require("multer");
+const path = require("path");
 
 dotenv.config();
 
@@ -29,10 +31,31 @@ mongoose.connection.once("open", () => {
 // }
 // run().catch(console.dir);
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images/post");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    res.status(200).json("File uploaded successfully.");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
